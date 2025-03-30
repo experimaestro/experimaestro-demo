0f8a39e3536c29bf7f773c7de78aa03fd0c2fb21
Benjamin Piwowarski <benjamin@piwowarski.fr>
2025-03-30 21:32:03 +0200

    Updated to latest datamaestro 1.4.0

--- mnist_xp/data.py
+++ mnist_xp/data.py
@@ -1,8 +1,9 @@
 from abc import ABC, abstractmethod
 from pathlib import Path
+import logging
 from torchvision.datasets import VisionDataset, MNIST
 from experimaestro import Meta, Param
-from datamaestro import dataset, Base
+from datamaestro import dataset, Base, Context
 from datamaestro.data.ml import Supervised
 from datamaestro.download.custom import custom_download
 
@@ -21,16 +22,16 @@ class MNISTLabelledImages(LabelledImages):
         return MNIST(self.root, train=self.train, **kwargs)
 
 
-def download_mnist(context, root: Path, force=False):
+def download_mnist(context: Context, root: Path, force=False):
+    logging.info("Downloading in %s", root)
     for train in [False, True]:
         MNIST(root, train=train, download=True)
 
 
+@custom_download("root", download_mnist)
 @dataset(id="com.lecun.mnist")
-def mnist() -> Supervised[LabelledImages, None, LabelledImages]:
+def mnist(root: Path) -> Supervised[LabelledImages, None, LabelledImages]:
     """This corresponds to a dataset with an ID `com.lecun.mnist`"""
-    root = custom_download(download_mnist)
-
     return Supervised(
         train=MNISTLabelledImages(root=root, train=True),
         test=MNISTLabelledImages(root=root, train=False),
